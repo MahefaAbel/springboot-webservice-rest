73d11fcf6467db290d2f4298f6149e38fab7850b
Mahefa Abel <abelmahefa@gmail.com>
2021-02-18 14:17:06 +0300

    update, refactor project*

---
+++ src/main/java/com/example/demo/Projects.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Projects {
+	@Id @GeneratedValue
+	private final long id;
+	private String title;
+	private String description;
+	private int company_id;
+	private int user_id;
+	
+	public Projects(
+			long id, 
+			String title,
+			String description,
+			int company_id,
+			int user_id
+	) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.company_id = company_id;
+		this.user_id = user_id;
+	}
+}
